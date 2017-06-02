de338d7a57f539f27105511d57a43e86397b45c2
Marcus Münger <marcus.munger@24money.se>
2017-06-02 13:32:47 +0200

    Removed json serialization from NonceToken

--- src/main/java/se/tfmoney/microservice/oauth/model/NonceToken.java
+++ src/main/java/se/tfmoney/microservice/oauth/model/NonceToken.java
@@ -1,9 +1,5 @@
 package se.tfmoney.microservice.oauth.model;
 
-import com.google.gson.annotations.Expose;
-import com.google.gson.annotations.SerializedName;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import se.tfmoney.microservice.oauth.util.database.jpa.Database;
 import se.tfmoney.microservice.oauth.util.nonce.NonceUtils;
 
@@ -11,8 +7,6 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
 import java.math.BigInteger;
 import java.security.SecureRandom;
 import java.text.DateFormat;
@@ -24,21 +18,13 @@ import java.util.Calendar;
  */
 @Entity
 @Table(name = "nonce")
-@XmlRootElement(name = "nonce_token")
-@ApiModel(description = "A nonce token that is used to deflect against replay attacks")
 public class NonceToken
 {
     @Id
-    @XmlElement(name = "nonce")
-    @SerializedName("nonce")
-    @ApiModelProperty(name = "nonce",
-                      value = "A token that is valid for one request. All requests need to supply a nonce token in order to deflect replay attacks",
-                      required = true)
     @Column(name = "token")
     public String token;
 
     @Column(name = "expiration_date")
-    @Expose(serialize = false, deserialize = false)
     public String expirationDate;
 
     public static NonceToken generateToken() throws Exception
